ee50d1e35ac88eea2faac5f2e53f976d3be0c106
laugel <laugel@2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad>
2006-10-11 19:34:44 +0000

    git-svn-id: https://svn.code.sf.net/p/hdgraph/code@66 2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad

--- HDGraphiqueur/HDGraphiqueurGUI/Program.cs
+++ HDGraphiqueur/HDGraphiqueurGUI/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace HDGraph
 {
@@ -12,9 +13,30 @@ namespace HDGraph
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            TraceSwitch mySwitch = new TraceSwitch("traceLevelSwitch", "HDG TraceSwitch");
+            try
+            {
+                Trace.WriteLineIf(mySwitch.TraceInfo, "Application started.");
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(PrintBug(ex));
+                System.Resources.ResourceManager res = new System.Resources.ResourceManager(typeof(Program).Assembly.GetName().Name + ".Resources.ApplicationMessages", typeof(Program).Assembly);
+                MessageBox.Show(res.GetString("CriticalError"),
+                                res.GetString("Error"), 
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static string PrintBug(Exception ex) {
+            string errMsg = ex.Message + " - Source: " + ex.Source + " - Stack: " + ex.StackTrace;
+            if (ex.InnerException != null)
+                return errMsg + " ==> " + PrintBug(ex.InnerException);
+            else
+                return errMsg;
         }
     }
 }
