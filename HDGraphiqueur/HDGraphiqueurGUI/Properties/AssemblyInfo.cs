891b620bdd9f16bf16c52c9fcaa10dd2740d5438
jyl2002 <jyl2002@2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad>
2007-01-22 20:29:11 +0000

    Updated installer for version 1.0.1
    git-svn-id: https://svn.code.sf.net/p/hdgraph/code@129 2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad

--- HDGraphiqueur/HDGraphiqueurGUI/Properties/AssemblyInfo.cs
+++ HDGraphiqueur/HDGraphiqueurGUI/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.4.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 //[assembly: NeutralResourcesLanguageAttribute("en")]
