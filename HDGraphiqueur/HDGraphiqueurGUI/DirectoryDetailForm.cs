2b7723bdf01fcda2fd85b7cb80ece9414c26e193
jyl2002 <jyl2002@2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad>
2007-09-13 20:58:53 +0000

    git-svn-id: https://svn.code.sf.net/p/hdgraph/code@171 2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad

--- HDGraphiqueur/HDGraphiqueurGUI/DirectoryDetailForm.cs
+++ HDGraphiqueur/HDGraphiqueurGUI/DirectoryDetailForm.cs
@@ -31,11 +31,23 @@ namespace HDGraph
                     throw new ArgumentNullException();
                 directory = value;
                 directoryDetailUC1.directoryNodeEntityBindingSource.DataSource = value;
-                directoryDetailUC1.directoryNodeListBindingSource.DataSource = new DirNodeSortableBindingList(value.Children);
+                IList<DirectoryNode> childrenListExceptHidenFreeSpace = ExcludeHidenFreeSpace(value.Children);
+                directoryDetailUC1.directoryNodeListBindingSource.DataSource = new DirNodeSortableBindingList(childrenListExceptHidenFreeSpace);
                 this.Text = String.Format(
                         HDGTools.resManager.GetString("DetailsForFolderFormTitle"),
                         directory.Name);
             }
+        }
+
+        private IList<DirectoryNode> ExcludeHidenFreeSpace(List<DirectoryNode> list)
+        {
+            List<DirectoryNode> resultList = new List<DirectoryNode>(list.Count);
+            foreach (DirectoryNode node in list)
+            {
+                if (node.DirectoryType != SpecialDirTypes.FreeSpaceAndHide)
+                    resultList.Add(node);
+            }
+            return resultList;
         }
 
     }
