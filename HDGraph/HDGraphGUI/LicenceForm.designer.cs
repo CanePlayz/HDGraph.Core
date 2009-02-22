eb6e2dddf76bb6e4126f118f1f06c2e3f869ee1f
jyl2002 <jyl2002@2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad>
2009-02-22 18:33:24 +0000

    Fixed : windows sizes
    git-svn-id: https://svn.code.sf.net/p/hdgraph/code@225 2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad

--- HDGraph/HDGraphGUI/LicenceForm.designer.cs
+++ HDGraph/HDGraphGUI/LicenceForm.designer.cs
@@ -37,8 +37,6 @@ namespace HDGraph
             // 
             // labelProductName
             // 
-            this.labelProductName.AccessibleDescription = null;
-            this.labelProductName.AccessibleName = null;
             resources.ApplyResources(this.labelProductName, "labelProductName");
             this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelProductName.Name = "labelProductName";
@@ -46,57 +44,39 @@ namespace HDGraph
             // 
             // labelCopyright
             // 
-            this.labelCopyright.AccessibleDescription = null;
-            this.labelCopyright.AccessibleName = null;
             resources.ApplyResources(this.labelCopyright, "labelCopyright");
-            this.labelCopyright.Font = null;
             this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelCopyright.Name = "labelCopyright";
             // 
             // textBoxDescription
             // 
-            this.textBoxDescription.AccessibleDescription = null;
-            this.textBoxDescription.AccessibleName = null;
             resources.ApplyResources(this.textBoxDescription, "textBoxDescription");
-            this.textBoxDescription.BackgroundImage = null;
-            this.textBoxDescription.Font = null;
             this.textBoxDescription.Name = "textBoxDescription";
             this.textBoxDescription.ReadOnly = true;
             this.textBoxDescription.TabStop = false;
             // 
             // okButton
             // 
-            this.okButton.AccessibleDescription = null;
-            this.okButton.AccessibleName = null;
             resources.ApplyResources(this.okButton, "okButton");
-            this.okButton.BackgroundImage = null;
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Font = null;
             this.okButton.Name = "okButton";
             // 
             // labelLicenceName
             // 
-            this.labelLicenceName.AccessibleDescription = null;
-            this.labelLicenceName.AccessibleName = null;
             resources.ApplyResources(this.labelLicenceName, "labelLicenceName");
             this.labelLicenceName.Name = "labelLicenceName";
             this.labelLicenceName.Click += new System.EventHandler(this.labelLicenceName_Click);
             // 
             // LicenceForm
             // 
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = null;
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.textBoxDescription);
             this.Controls.Add(this.labelProductName);
             this.Controls.Add(this.labelCopyright);
             this.Controls.Add(this.labelLicenceName);
-            this.Font = null;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LicenceForm";
