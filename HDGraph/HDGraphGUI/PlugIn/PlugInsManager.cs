c7b92e090c3a2461e0538878176695ade58886fb
jyl2002 <jyl2002@2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad>
2009-08-31 21:38:26 +0000

    Update : plugin detection
    git-svn-id: https://svn.code.sf.net/p/hdgraph/code@302 2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad

--- HDGraph/HDGraphGUI/PlugIn/PlugInsManager.cs
+++ HDGraph/HDGraphGUI/PlugIn/PlugInsManager.cs
@@ -5,31 +5,73 @@ using System.Reflection;
 using HDGraph.Interfaces.DrawEngines;
 using System.Windows.Forms;
 using HDGraph.Interfaces.ScanEngines;
+using System.IO;
+using System.Diagnostics;
 
 namespace HDGraph.PlugIn
 {
     public class PlugInsManager
     {
-        public static void Test(IDirectoryNode node, DrawOptions options)
+        private const string PlugInRelativePath = "Plugins";
+
+        /// <summary>
+        /// List all available Draw Engine Plugins.
+        /// </summary>
+        /// <returns></returns>
+        public static List<IDrawEngineContract> GetDrawEnginePlugins()
         {
-            Assembly assembly = Assembly.Load("WpfDrawEngine");
-            foreach (Type type in assembly.GetTypes())
+            if (!Directory.Exists(PlugInRelativePath))
+                return new List<IDrawEngineContract>();
+
+            List<IDrawEngineContract> plugInsList = new List<IDrawEngineContract>();
+            foreach (string fileName in Directory.GetFiles(PlugInRelativePath, "*.dll", SearchOption.TopDirectoryOnly))
             {
-                if (typeof(IDrawEngineContract).IsAssignableFrom(type))
+                try
                 {
-                    IDrawEngineContract engineContract = (IDrawEngineContract)Activator.CreateInstance(type);
-                    IControlTypeEngine engine = engineContract.GetNewControlTypeEngine();
-                    Control control = engine.GenerateControlFromNode(node, options);
-                    Form form = new Form();
-                    form.WindowState = FormWindowState.Maximized;
-                    form.Controls.Add(control);
-                    control.Dock = DockStyle.Fill;
-                    control.Margin = new Padding(10);
-                    control.Padding = new Padding(10);
-                    form.Show();
-                    return;
+                    Assembly assembly = Assembly.LoadFile(new FileInfo(fileName).FullName);
+                    foreach (Type type in assembly.GetTypes())
+                    {
+                        try
+                        {
+                            if (type.IsPublic && typeof(IDrawEngineContract).IsAssignableFrom(type))
+                            {
+                                IDrawEngineContract engineContract = (IDrawEngineContract)Activator.CreateInstance(type);
+                                plugInsList.Add(engineContract);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Trace.TraceError(HDGTools.PrintError(ex));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Trace.TraceError(HDGTools.PrintError(ex));
                 }
             }
+            return plugInsList;
+        }
+
+
+        public static void Test(IDirectoryNode node, DrawOptions options)
+        {
+            List<IDrawEngineContract> plugins = GetDrawEnginePlugins();
+            if (plugins == null
+                || plugins.Count == 0)
+                return;
+
+            IDrawEngineContract engineContract = plugins[0];
+            IControlTypeEngine engine = engineContract.GetNewControlTypeEngine();
+            Control control = engine.GenerateControlFromNode(node, options);
+            Form form = new Form();
+            form.WindowState = FormWindowState.Maximized;
+            form.Controls.Add(control);
+            control.Dock = DockStyle.Fill;
+            control.Margin = new Padding(10);
+            control.Padding = new Padding(10);
+            form.Show();
+            return;
         }
 
     }
