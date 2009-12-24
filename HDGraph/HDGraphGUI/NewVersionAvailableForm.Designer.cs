359ed3b0ccaf99f3dcc4b0f7f0d2ed43df777ca8
jyl2002 <jyl2002@2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad>
2009-12-24 17:50:26 +0000

    Added : "New version auto-check" i18n.
    git-svn-id: https://svn.code.sf.net/p/hdgraph/code@349 2dc5f10e-1ed8-0143-a4cc-3b574b11e1ad

--- HDGraph/HDGraphGUI/NewVersionAvailableForm.Designer.cs
+++ HDGraph/HDGraphGUI/NewVersionAvailableForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewVersionAvailableForm));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,129 +46,120 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 9);
+            this.label1.AccessibleDescription = null;
+            this.label1.AccessibleName = null;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Font = null;
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "A newer version of HDGraph is available.";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(60, 32);
+            this.label2.AccessibleDescription = null;
+            this.label2.AccessibleName = null;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Font = null;
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Your version is :";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(60, 55);
+            this.label3.AccessibleDescription = null;
+            this.label3.AccessibleName = null;
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Font = null;
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "The new version is :";
             // 
             // labelCurrentVersion
             // 
-            this.labelCurrentVersion.AutoSize = true;
-            this.labelCurrentVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCurrentVersion.Location = new System.Drawing.Point(171, 32);
+            this.labelCurrentVersion.AccessibleDescription = null;
+            this.labelCurrentVersion.AccessibleName = null;
+            resources.ApplyResources(this.labelCurrentVersion, "labelCurrentVersion");
             this.labelCurrentVersion.Name = "labelCurrentVersion";
-            this.labelCurrentVersion.Size = new System.Drawing.Size(55, 13);
-            this.labelCurrentVersion.TabIndex = 1;
-            this.labelCurrentVersion.Text = "[1.1.1.1]";
             // 
             // labelNewerVersion
             // 
-            this.labelNewerVersion.AutoSize = true;
-            this.labelNewerVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNewerVersion.Location = new System.Drawing.Point(171, 55);
+            this.labelNewerVersion.AccessibleDescription = null;
+            this.labelNewerVersion.AccessibleName = null;
+            resources.ApplyResources(this.labelNewerVersion, "labelNewerVersion");
             this.labelNewerVersion.Name = "labelNewerVersion";
-            this.labelNewerVersion.Size = new System.Drawing.Size(55, 13);
-            this.labelNewerVersion.TabIndex = 1;
-            this.labelNewerVersion.Text = "[1.1.1.1]";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.AccessibleDescription = null;
+            this.label4.AccessibleName = null;
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Font = null;
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(477, 13);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Its is recommended to update to the new version. Would you like to go to the down" +
-                "load page now ?";
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 77);
+            this.label5.AccessibleDescription = null;
+            this.label5.AccessibleName = null;
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Font = null;
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(350, 13);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "What\'s new in the new version ? New features and bugfixes are listed on";
             // 
             // buttonYes
             // 
+            this.buttonYes.AccessibleDescription = null;
+            this.buttonYes.AccessibleName = null;
+            resources.ApplyResources(this.buttonYes, "buttonYes");
+            this.buttonYes.BackgroundImage = null;
             this.buttonYes.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonYes.Location = new System.Drawing.Point(195, 126);
+            this.buttonYes.Font = null;
             this.buttonYes.Name = "buttonYes";
-            this.buttonYes.Size = new System.Drawing.Size(75, 23);
-            this.buttonYes.TabIndex = 2;
-            this.buttonYes.Text = "Yes";
             this.buttonYes.UseVisualStyleBackColor = true;
             this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
             // 
             // buttonNo
             // 
+            this.buttonNo.AccessibleDescription = null;
+            this.buttonNo.AccessibleName = null;
+            resources.ApplyResources(this.buttonNo, "buttonNo");
+            this.buttonNo.BackgroundImage = null;
             this.buttonNo.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonNo.Location = new System.Drawing.Point(276, 126);
+            this.buttonNo.Font = null;
             this.buttonNo.Name = "buttonNo";
-            this.buttonNo.Size = new System.Drawing.Size(75, 23);
-            this.buttonNo.TabIndex = 2;
-            this.buttonNo.Text = "No";
             this.buttonNo.UseVisualStyleBackColor = true;
             this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
             // 
             // linkLabel1
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(362, 77);
+            this.linkLabel1.AccessibleDescription = null;
+            this.linkLabel1.AccessibleName = null;
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.Font = null;
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(170, 13);
-            this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "this page of the HDGraph website.";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.AccessibleDescription = null;
+            this.pictureBox1.AccessibleName = null;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.BackgroundImage = null;
+            this.pictureBox1.Font = null;
             this.pictureBox1.Image = global::HDGraph.Properties.Resources.Info;
-            this.pictureBox1.Location = new System.Drawing.Point(15, 9);
+            this.pictureBox1.ImageLocation = null;
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 28);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // labelReleaseDate
             // 
-            this.labelReleaseDate.AutoSize = true;
-            this.labelReleaseDate.Location = new System.Drawing.Point(232, 55);
+            this.labelReleaseDate.AccessibleDescription = null;
+            this.labelReleaseDate.AccessibleName = null;
+            resources.ApplyResources(this.labelReleaseDate, "labelReleaseDate");
+            this.labelReleaseDate.Font = null;
             this.labelReleaseDate.Name = "labelReleaseDate";
-            this.labelReleaseDate.Size = new System.Drawing.Size(105, 13);
-            this.labelReleaseDate.TabIndex = 1;
-            this.labelReleaseDate.Text = "released on [TODO].";
             // 
             // NewVersionAvailableForm
             // 
             this.AcceptButton = this.buttonYes;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(547, 162);
+            this.BackgroundImage = null;
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.buttonNo);
@@ -180,12 +172,12 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
+            this.Font = null;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "NewVersionAvailableForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "New version available !";
             this.Load += new System.EventHandler(this.NewVersionAvailableForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
