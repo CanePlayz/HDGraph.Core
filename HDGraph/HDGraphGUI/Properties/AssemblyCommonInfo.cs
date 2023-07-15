df0c531916323770b64856ad3d9ac6d395c08d3c
Jacob P <jjpfst@gmail.com>
2023-07-15 03:35:06 +0200

    First .NET 7.0 version

--- HDGraph/HDGraphGUI/Properties/AssemblyCommonInfo.cs
+++ HDGraph/HDGraphGUI/Properties/AssemblyCommonInfo.cs
@@ -2,18 +2,4 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
-
-
-[assembly: AssemblyCompany("HDGraph.com")]
-[assembly: AssemblyProduct("HDGraph")]
-[assembly: AssemblyCopyright("Copyright © 2006-2015 LAUGEL Jean-Yves")]
 [assembly: AssemblyTrademark("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.5.1.0")]
--- HDGraph/Win32NativeFileSystemEnumerator/Properties/AssemblyInfo.cs
+++ HDGraph/Win32NativeFileSystemEnumerator/Properties/AssemblyInfo.cs
@@ -1,13 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Win32NativeFileSystemEnumerator")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
